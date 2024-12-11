43ff64fbd2611eccc0373284dcbb14e8d59b06a3
unknown <abhishekmishra992016@gmail.com>
2024-12-11 10:27:07 +0530

    minor changes

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -123,7 +123,7 @@ const getUserById = async (req, res) => {
 /* -------------------------------------------------------------------------- */
 const getAllUsers = async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await User.find().select('-password');
         res.status(200).json({ users });
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
