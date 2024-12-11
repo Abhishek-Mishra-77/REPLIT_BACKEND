f3a00e5e93efa0ceae99ba3db9ccb37af7519b3c
unknown <abhishekmishra992016@gmail.com>
2024-12-11 17:38:10 +0530

    working on langauge schema

--- controllers/langaugeController.js
+++ controllers/langaugeController.js
@@ -1,15 +1,35 @@
 import Langauge from "../models/langaugeModel.js";
 
-
 /* -------------------------------------------------------------------------- */
 /*                           CREATE LANGAUGE                                  */
 /* -------------------------------------------------------------------------- */
-const createLangauge = async (req, res) => { }
+const createLangauge = async (req, res) => {
+    const { name } = req.body;
+    try {
+
+        if (!name) return res.status(400).json({ message: "Name is required" });
+
+        const existingLangauge = await Langauge.findOne({ name });
+
+        if (existingLangauge) return res.status(400).json({ message: "Langauge already exists" });
+
+        const newLangauge = new Langauge({ name });
+
+        await newLangauge.save();
+
+        res.status(201).json({ message: "Langauge created successfully", langauge: newLangauge });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
 
 /* -------------------------------------------------------------------------- */
 /*                           REMOVE LANGAUGE                                  */
 /* -------------------------------------------------------------------------- */
-const removeLangauge = async (req, res) => { }
+const removeLangauge = async (req, res) => {
+
+
+}
 
 /* -------------------------------------------------------------------------- */
 /*                           UPDATE LANGAUGE                                  */
